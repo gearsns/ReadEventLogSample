9fe5549c43509832b9d8de25c9dfbac081f932f8
gears <gears.ns@gmail.com>
2024-11-11 12:49:32 +0900

    検索条件をパラメータに

--- ReadEventLog.cpp
+++ ReadEventLog.cpp
@@ -11,7 +11,12 @@
 #pragma comment(lib, "advapi32.lib")
 #pragma comment(lib, "Shlwapi.lib")
 
-static void checkEventLog(LPCTSTR filename)
+static LPCTSTR EVENT_NAME_4624 = L"An account was successfully logged on.";
+static LPCTSTR EVENT_NAME_4625 = L"An account failed to log on.";
+static LPCTSTR EVENT_NAME_4771 = L"Kerberos pre-authentication failed.";
+static LPCTSTR EVENT_NAME_4776 = L"The computer attempted to validate the credentials for an account.";
+
+static void checkEventLog(LPCTSTR check_str, LPCTSTR filename, LPCTSTR dispfilename)
 {
     HANDLE hEvent = OpenBackupEventLog(NULL, filename);
     if (NULL == hEvent)
@@ -28,6 +33,7 @@ static void checkEventLog(LPCTSTR filename)
             CloseEventLog(hEvent);
             return;
         }
+        int len = lstrlen(check_str);
         LPBYTE buffer = (LPBYTE)GlobalLock(hBuffer);
         if (buffer == NULL)
         {
@@ -50,44 +56,138 @@ static void checkEventLog(LPCTSTR filename)
                         Kerberos 事前認証に失敗しました。: 4771
                         コンピューターがアカウントの資格情報の確認を試行しました。 : 4776
                     */
-                    TCHAR lpEventName[512] = {};
+                    LPCTSTR lpEventName = nullptr;
                     bool bFind = false;
                     LPCTSTR lpString = (LPCTSTR)((PBYTE)pEvtRec + pEvtRec->StringOffset);
-                    int target = -1;
+                    int target_user = -1;
+                    int target_ipadr = -1;
+                    int target_workstation = -1;
+                    int numStrings = pEvtRec->NumStrings;
                     if (pEvtRec->EventID == 4624)
                     {
-                        target = 5;
-                        lstrcpy(lpEventName, L"An account was successfully logged on.");
-                    }
-                    else if (pEvtRec->EventID == 4771)
-                    {
-                        target = 0;
-                        lstrcpy(lpEventName, L"Kerberos pre-authentication failed.");
-                    }
-                    else if (pEvtRec->EventID == 4776)
-                    {
-                        target = 1;
-                        lstrcpy(lpEventName, L"The computer attempted to validate the credentials for an account.");
+                        target_user = 5;
+                        target_ipadr = 18;
+                        numStrings = 19;
+                        lpEventName = EVENT_NAME_4624;
+                        /*
+                            0<Data Name="SubjectUserSid">S-1-5-18</Data>
+                            1<Data Name="SubjectUserName">WIN-GG82ULGC9GO$</Data>
+                            2<Data Name="SubjectDomainName">WORKGROUP</Data>
+                            3<Data Name="SubjectLogonId">0x3e7</Data>
+                            4<Data Name="TargetUserSid">S-1-5-21-1377283216-344919071-3415362939-500</Data>
+                            5<Data Name="TargetUserName">Administrator</Data>
+                            6<Data Name="TargetDomainName">WIN-GG82ULGC9GO</Data>
+                            7<Data Name="TargetLogonId">0x8dcdc</Data>
+                            8<Data Name="LogonType">2</Data>
+                            9<Data Name="LogonProcessName">User32</Data>
+                            0<Data Name="AuthenticationPackageName">Negotiate</Data>
+                            1<Data Name="WorkstationName">WIN-GG82ULGC9GO</Data>
+                            2<Data Name="LogonGuid">{00000000-0000-0000-0000-000000000000}</Data>
+                            3<Data Name="TransmittedServices">-</Data>
+                            4<Data Name="LmPackageName">-</Data>
+                            5<Data Name="KeyLength">0</Data>
+                            6<Data Name="ProcessId">0x44c</Data>
+                            7<Data Name="ProcessName">C:\\Windows\\System32\\svchost.exe</Data>
+                            8<Data Name="IpAddress">127.0.0.1</Data>
+                            9<Data Name="IpPort">0</Data>
+                            0<Data Name="ImpersonationLevel">%%1833</Data>
+                            1<Data Name="RestrictedAdminMode">-</Data>
+                            2<Data Name="TargetOutboundUserName">-</Data>
+                            3<Data Name="TargetOutboundDomainName">-</Data>
+                            4<Data Name="VirtualAccount">%%1843</Data>
+                            5<Data Name="TargetLinkedLogonId">0x0</Data>
+                            6<Data Name="ElevatedToken">%%1842</Data>
+                        */
                     }
                     else if (pEvtRec->EventID == 4625)
                     {
-                        target = 5;
-                        lstrcpy(lpEventName, L"An account failed to log on.");
+                        target_user = 5;
+                        numStrings = 6;
+                        lpEventName = EVENT_NAME_4625;
+                        /*
+                             <Data Name="SubjectUserSid">S-1-5-18</Data>
+                             <Data Name="SubjectUserName">DC01$</Data>
+                             <Data Name="SubjectDomainName">CONTOSO</Data>
+                             <Data Name="SubjectLogonId">0x3e7</Data>
+                             <Data Name="TargetUserSid">S-1-0-0</Data>
+                             <Data Name="TargetUserName">Auditor</Data>
+                             <Data Name="TargetDomainName">CONTOSO</Data>
+                             <Data Name="Status">0xc0000234</Data>
+                             <Data Name="FailureReason">%%2307</Data>
+                             <Data Name="SubStatus">0x0</Data>
+                             <Data Name="LogonType">2</Data>
+                             <Data Name="LogonProcessName">User32</Data>
+                             <Data Name="AuthenticationPackageName">Negotiate</Data>
+                             <Data Name="WorkstationName">DC01</Data>
+                             <Data Name="TransmittedServices">-</Data>
+                             <Data Name="LmPackageName">-</Data>
+                             <Data Name="KeyLength">0</Data>
+                             <Data Name="ProcessId">0x1bc</Data>
+                             <Data Name="ProcessName">C:\\Windows\\System32\\winlogon.exe</Data>
+                             <Data Name="IpAddress">127.0.0.1</Data>
+                             <Data Name="IpPort">0</Data>
+                        */
                     }
-                    // ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-                    // TargetUserNameが'AA'で始まる物を対象
-                    // この辺を適当に変える!!
-                    // ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-                    for (int i = 0; i < pEvtRec->NumStrings; i++)
+                    else if (pEvtRec->EventID == 4771)
                     {
-                        if (target == i)
+                        target_user = 0;
+                        target_ipadr = 6;
+                        numStrings = 7;
+                        lpEventName = EVENT_NAME_4771;
+                        /*
+                             <Data Name="TargetUserName">dadmin</Data>
+                             <Data Name="TargetSid">S-1-5-21-3457937927-2839227994-823803824-1104</Data>
+                             <Data Name="ServiceName">krbtgt/CONTOSO.LOCAL</Data>
+                             <Data Name="TicketOptions">0x40810010</Data>
+                             <Data Name="Status">0x10</Data>
+                             <Data Name="PreAuthType">15</Data>
+                             <Data Name="IpAddress">::ffff:10.0.0.12</Data>
+                             <Data Name="IpPort">49254</Data>
+                             <Data Name="CertIssuerName" />
+                             <Data Name="CertSerialNumber" />
+                             <Data Name="CertThumbprint" />
+                        */
+                    }
+                    else if (pEvtRec->EventID == 4776)
+                    {
+                        target_user = 1;
+                        target_workstation = 2;
+                        numStrings = 3;
+                        lpEventName = EVENT_NAME_4776;
+                        /*
+                             <Data Name="PackageName">MICROSOFT\_AUTHENTICATION\_PACKAGE\_V1\_0</Data>
+                             <Data Name="TargetUserName">dadmin</Data>
+                             <Data Name="Workstation">WIN81</Data>
+                             <Data Name="Status">0xc0000234</Data>
+                        */
+                    }
+                    if (pEvtRec->NumStrings < numStrings)
+                    {
+                        numStrings = pEvtRec->NumStrings;
+                    }
+                    if (target_user >= 0 || target_ipadr >= 0 || target_workstation >= 0)
+                    {
+                        for (int i = 0; i < numStrings; i++)
                         {
-                            if (lpString[0] == L'A' && lpString[1] == L'A') {
-                                bFind = true;
+                            if (target_user == i || target_workstation == i)
+                            {
+                                if (lpString[0] == check_str[0] && StrCmpN(lpString, check_str, len) == 0) {
+                                    bFind = true;
+                                    break;
+                                }
                             }
-                            break;
+                            else if (target_ipadr == i)
+                            {
+                                if (lpString[0] == L':'
+                                    && StrCmpN(lpString, L"::ffff:"/*IPV4*/, (sizeof(L"::ffff:") / sizeof(TCHAR)) - 1) == 0
+                                    && StrCmpN(lpString + (sizeof(L"::ffff:") / sizeof(TCHAR)) - 1, check_str, len) == 0
+                                    ) {
+                                    bFind = true;
+                                    break;
+                                }
+                            }
+                            lpString += lstrlen(lpString) + 1;
                         }
-                        lpString += lstrlen(lpString) + 1;
                     }
                     if (bFind)
                     {
@@ -95,8 +195,9 @@ static void checkEventLog(LPCTSTR filename)
                         struct tm local;
                         localtime_s(&local, &t);
                         //
-                        std::wcout << pEvtRec->EventID << "[" << lpEventName << "],";
-                        std::cout << std::put_time(&local, "%Y-%m-%d %H:%M:%S") << ",";
+                        std::wcout << dispfilename;
+                        std::cout << "," << std::put_time(&local, "%Y-%m-%d %H:%M:%S") << ",";
+                        std::wcout << pEvtRec->EventID << "," << lpEventName << ",";
                         lpString = (LPCTSTR)((PBYTE)pEvtRec + pEvtRec->StringOffset);
                         for (int i = 0; i < pEvtRec->NumStrings; i++)
                         {
@@ -122,7 +223,18 @@ static void checkEventLog(LPCTSTR filename)
 
 using namespace cppunzip;
 
-static bool ZipFolder(LPCTSTR lpInZipFile)
+static std::wstring StringToWString(const std::string& str)
+{
+    // 変換に必要なサイズを取得
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), static_cast<int>(str.size()), NULL, 0);
+    // 変換先のwchar_t配列を作成
+    std::wstring wstr(size_needed, 0);
+    // 変換を実行
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), static_cast<int>(str.size()), &wstr[0], size_needed);
+    return wstr;
+}
+
+static bool ZipFolder(LPCTSTR check_str, LPCTSTR lpInZipFile)
 {
     bool ret = true;
 
@@ -143,7 +255,8 @@ static bool ZipFolder(LPCTSTR lpInZipFile)
                 {
                     fout.write((char*)&content[0], content.size() * sizeof(content[0]));
                     fout.close();
-                    checkEventLog(filename);
+                    std::wstring wfilename = StringToWString(fileEntry.fileName());
+                    checkEventLog(check_str, filename, PathFindFileName(wfilename.c_str()));
                 }
                 DeleteFile(filename);
             }
@@ -157,7 +270,7 @@ static bool ZipFolder(LPCTSTR lpInZipFile)
     return ret;
 }
 
-static void Check(LPCTSTR filename)
+static void Check(LPCTSTR check_str, LPCTSTR filename)
 {
     if (PathIsDirectory(filename))
     {
@@ -181,7 +294,7 @@ static void Check(LPCTSTR filename)
                 std::wcerr << win32fd.cFileName << std::endl;
                 TCHAR filename[MAX_PATH] = {};
                 swprintf_s(filename, L"%s/%s", dir_name, win32fd.cFileName);
-                checkEventLog(filename);
+                checkEventLog(check_str, filename, win32fd.cFileName);
 
             }
         } while (FindNextFile(hFind, &win32fd));
@@ -194,22 +307,22 @@ static void Check(LPCTSTR filename)
     {
         if (lstrcmpi(lpExt, L".zip") == 0)
         {
-            ZipFolder(filename);
+            ZipFolder(check_str, filename);
         }
         else if (lstrcmpi(lpExt, L".evtx") == 0)
         {
-            checkEventLog(filename);
+            checkEventLog(check_str, filename, PathFindFileName(filename));
         }
     }
 }
 
 int wmain(int argc, const wchar_t* argv[])
 {
-    if (argc <= 1)
+    if (argc <= 2)
     {
         return -1;
     }
-    Check(argv[1]);
+    Check(argv[1], argv[2]);
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
